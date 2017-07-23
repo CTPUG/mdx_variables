6cac02de0355b4534079e0d74913d1d511776842
Simon Cross <hodgestar@gmail.com>
2017-07-23 23:14:20 +0200

    Add initial implementation and tests.

---
+++ mdx_variables.py
@@ -0,0 +1,65 @@
+""" Variables extension for Python Markdown.
+"""
+
+from markdown import Extension
+from markdown.inlinepatterns import Pattern
+
+
+class VariablePattern(Pattern):
+    """ Handle matches to the variable pattern (i.e. ${...}). """
+
+    VARIABLE_PATTERN = r'\$\{(\w+)\}'
+    CONF_DEFAULTS = {
+        'vars': None,
+    }
+
+    def __init__(self, conf):
+        super(VariablePattern, self).__init__(self.VARIABLE_PATTERN)
+        conf_vars = self.CONF_DEFAULTS.copy()
+        conf_vars.update(conf or {})
+        self.getters = conf_vars['vars'] or {}
+        self.missing_getter = self.getters.pop(
+            '__getattr__', self._default_missing)
+
+    def _default_missing(self, var_name):
+        return "MISSING VARIABLE: %s" % (var_name,)
+
+    def _default_error(self, var_name):
+        return "BAD VARIABLE: %s" % (var_name,)
+
+    def _catch_error(self, var_name, f, *args):
+        try:
+            return f(*args)
+        except:
+            pass
+        return self._default_error(var_name)
+
+    def _get_value(self, var_name):
+        if var_name not in self.getters:
+            return self._catch_error(var_name, self.missing_getter, var_name)
+        value = self.getters[var_name]
+        if callable(value):
+            value = self._catch_error(var_name, value)
+        return value
+
+    def handleMatch(self, m):
+        var_name = m.group(2)
+        return self._get_value(var_name)
+
+
+class VariablesExtension(Extension):
+    """ The variables markdown extension. """
+
+    def __init__(self, configs):
+        self.conf = configs
+
+    def extendMarkdown(self, md, md_globals):
+        """ Initializes markdown extension components. """
+        md.inlinePatterns.add(
+            'variable', VariablePattern(self.conf),
+            '<not_strong')  # add after the not_strong pattern
+
+
+def makeExtension(configs=None):
+    """ Initialize the variables extension. """
+    return VariablesExtension(configs=configs)
---
+++ tests.py
@@ -0,0 +1,108 @@
+from unittest import TestCase
+
+import xmltodict
+
+from markdown import Markdown
+from markdown.util import etree
+
+from mdx_variables import VariablesExtension, VariablePattern, makeExtension
+
+
+class XmlTestCaseMixin(object):
+    """ Helper class for asserting that XML documents describe the same XML
+        structures.
+    """
+
+    def mk_doc(self, s):
+        return etree.fromstring(
+            "<div>" + s.strip() + "</div>")
+
+    def assert_xml_equal(self, a, b):
+        self.assertEqual(
+            xmltodict.parse(etree.tostring(a)),
+            xmltodict.parse(etree.tostring(b)))
+
+    def assert_xmltext_equal(self, a, b):
+        self.assert_xml_equal(self.mk_doc(a), self.mk_doc(b))
+
+
+class TestVariablesPattern(XmlTestCaseMixin, TestCase):
+    """ Test VariablesPattern and handling of variables. """
+
+    def mk_markdown(self, conf):
+        md = Markdown()
+        ext = VariablesExtension(conf)
+        ext.extendMarkdown(md, {})
+        return md
+
+    def test_known_variable(self):
+        def bar():
+            return "bar"
+        md = self.mk_markdown({'vars': {'foo': bar}})
+        xml = md.convert("${foo}")
+        self.assert_xmltext_equal(xml, "<p>bar</p>")
+
+    def test_bad_variable(self):
+        def err():
+            raise RuntimeError("should be caught")
+        md = self.mk_markdown({'vars': {'foo': err}})
+        xml = md.convert("${foo}")
+        self.assert_xmltext_equal(xml, "<p>BAD VARIABLE: foo</p>")
+
+    def test_unknown_variable_without_default(self):
+        md = self.mk_markdown({'vars': {}})
+        xml = md.convert("${foo}")
+        self.assert_xmltext_equal(xml, "<p>MISSING VARIABLE: foo</p>")
+
+    def test_unknown_variable_with_default(self):
+        def default(varname):
+            return "got %s" % (varname,)
+        md = self.mk_markdown({'vars': {'__getattr__': default}})
+        xml = md.convert("${foo}")
+        self.assert_xmltext_equal(xml, "<p>got foo</p>")
+
+    def test_bad_default(self):
+        def default_err(varname):
+            raise RuntimeError("Should be caught")
+        md = self.mk_markdown({'vars': {'__getattr__': default_err}})
+        xml = md.convert("${foo}")
+        self.assert_xmltext_equal(xml, "<p>BAD VARIABLE: foo</p>")
+
+
+class TestVariablesExtension(TestCase):
+    """ Test VariablesExtension class. """
+
+    def mk_markdown(self):
+        md = Markdown()
+        md_globals = {}
+        return md, md_globals
+
+    def assert_registered(self, md, md_globals):
+        pattern = md.inlinePatterns['variable']
+        self.assertTrue(isinstance(pattern, VariablePattern))
+        self.assertEqual(md_globals, {})
+
+    def assert_not_registered(self, md, md_globals):
+        self.assertFalse('variable' in md.inlinePatterns)
+        self.assertEqual(md_globals, {})
+
+    def text_create(self):
+        ext = VariablesExtension({'a': 'b'})
+        self.assertEqual(ext.conf, {'a': 'b'})
+
+    def test_extend_markdown(self):
+        md, md_globals = self.mk_markdown()
+        ext = VariablesExtension({})
+        self.assert_not_registered(md, md_globals)
+        ext.extendMarkdown(md, md_globals)
+        self.assert_registered(md, md_globals)
+
+
+class TestExtensionRegistration(TestCase):
+    """ Test registration of variables extension. """
+
+    def test_make_extension(self):
+        configs = {'a': 'b'}
+        ext = makeExtension(configs)
+        self.assertTrue(isinstance(ext, VariablesExtension))
+        self.assertEqual(ext.conf, configs)
