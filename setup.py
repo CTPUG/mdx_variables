644213678fcbbdaeaff9c7084739d600969c232a
Simon Cross <hodgestar@gmail.com>
2017-07-24 18:53:11 +0200

    Version 0.1.3.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="mdx-variables",
-    version="0.1.2",
+    version="0.1.3",
     url='http://github.com/CTPUG/mdx_variables',
     license='MIT',
     description="A variables extension for Markdown",
